7f7cb7e1627ac8ca22f6f360f8f869dc66c3e94d
Betheval <miguelgutp@gmail.com>
2024-06-09 16:26:41 +0200

    changes in tf

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -2,16 +2,6 @@ variable "aws_region" {
   description = "The AWS region to create resources"
   default     = "eu-west-1"
 }
-variable "github_repo_url" {
-  description = "The URL of the GitHub Repo"
-  default     = "https://github.com/Betheval/Cantaso"
-
-}
-variable "codebuild_project_name" {
-  description = "The name of the codebuild project"
-  default     = "codebuild_cantaso"
-
-}
 
 variable "aws_ecr_repository" {
   description = "The name of the ECR repository"
